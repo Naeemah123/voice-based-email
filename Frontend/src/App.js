cb3b53e1485e451d3231900be1c069865ecd4f9f
Naeemah123 <154915258+Naeemah123@users.noreply.github.com>
2025-04-05 21:12:52 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Welcome from "./welcome"
 import Email from "./email"
-import Axios from "axios"
+import axios from "axios"
 import {SUCCESS} from "./error_codes"
 
 class App extends React.Component {
